d5be1e77a9d564bc493440cdf3cbad57e9359d54
Jose Olivarria <145067078+jolivarria1212@users.noreply.github.com>
2024-02-23 13:51:34 -0800

    I finished

--- P5PlayerControlOlivarria/Assets/Scripts/OncomingTanks.cs
+++ P5PlayerControlOlivarria/Assets/Scripts/OncomingTanks.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowPlayer : MonoBehaviour
+public class OncomingTanks : MonoBehaviour
 {
-   public GameObject player;
-   private Vector3 offset = new Vector3(0, 5, -7);
     // Start is called before the first frame update
     void Start()
     {
@@ -13,9 +11,9 @@ public class FollowPlayer : MonoBehaviour
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
-        transform.position = player.transform.position + offset;
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
         
     }
 }
--- P5PlayerControlOlivarria/Assets/Scripts/PlayerController.cs
+++ P5PlayerControlOlivarria/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    public Camera Maincamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +21,18 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Move the vehicle foward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //Moves the car forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        if (Input.GetKeyDown(switchKey))
+        {
+            Maincamera.enabled = !Maincamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+
+        }
         
     }
 }
